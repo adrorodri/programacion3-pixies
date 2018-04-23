5760c13ea2e97fa8caed4c648400f791d70bee5d
MateoAltamiranoVega <mateoaltamiaranovega1998@gmail.com>
2018-04-23 19:49:53 -0400

    Flujo registrarse y validacion

--- app/src/main/java/com/programacion3/pixies/app/RegisterActivity.java
+++ app/src/main/java/com/programacion3/pixies/app/RegisterActivity.java
@@ -4,22 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Button Registrarse_2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_activity);
-        Registrarse_2 = findViewById(R.id.registrarse_2);
-        Registrarse_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_register);
+    }
+    public void clickButton(View view) {
+        Intent intent = new  Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
