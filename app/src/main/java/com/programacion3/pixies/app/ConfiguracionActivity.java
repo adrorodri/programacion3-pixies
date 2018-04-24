9311b46da866c8d3d6dde716ccbf8392c7a516b4
MateoAltamiranoVega <mateoaltamiaranovega1998@gmail.com>
2018-04-23 21:17:03 -0400

    Flujo perfil drawer y password oculto

--- app/src/main/java/com/programacion3/pixies/app/ConfiguracionActivity.java
+++ app/src/main/java/com/programacion3/pixies/app/ConfiguracionActivity.java
@@ -1,16 +1,13 @@
 package com.programacion3.pixies.app;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class PerfilActivity extends DrawerActivity {
-   @Override
+public class ConfiguracionActivity extends DrawerActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil);
+        setContentView(R.layout.activity_configuracion);
     }
-
 }
