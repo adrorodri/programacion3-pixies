0936bfd11f059a032cef375f8a2e0e71c8caacd4
MateoAltamiranoVega <mateoaltamiaranovega1998@gmail.com>
2018-05-04 12:44:45 -0400

    Nuevo bienvenidos activity

--- app/src/main/java/com/programacion3/pixies/app/BienvenidosActivity.java
+++ app/src/main/java/com/programacion3/pixies/app/BienvenidosActivity.java
@@ -13,30 +13,11 @@ import java.util.List;
 
 public class BienvenidosActivity extends DrawerActivity{
 
-    List<Producto> productoList = new LinkedList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recycler_view_layout);
+        setContentView(R.layout.activity_bienvenidos);
 
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-        productoList.add(new Producto(R.drawable.alcancia,10,20,"producto1"));
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,productoList);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
-        recyclerView.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/programacion3/pixies/app/MainActivity.java
+++ app/src/main/java/com/programacion3/pixies/app/MainActivity.java
@@ -16,78 +16,80 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextPassword;
     String emailValue;
     String passwordValue;
-
+    String[] validEmail ={"pixies@gmail.com"};
+    String[] validPassword={"123456"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
-        final EditText etEmail  = (EditText) findViewById(R.id.editTextEmail);
-        final EditText etPassword = (EditText) findViewById(R.id.editTextPassword);
-        Button btnLogin = (Button) findViewById(R.id.entrar);
-        Button btnRegister = (Button) findViewById(R.id.registrarse);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String Email = etEmail.getText().toString();
-                String Contrasena = etPassword.getText().toString();
-
-                SharedPreferences preferences = getPreferences("MYPREFS", MODE_PRIVATE);
-
-                String emailDetalles = preferences.getString(Email + Contrasena + "data", "Email o Contrasena es incorrecto.");
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.commit();
-
-                Intent mainact = new Intent(MainActivity.this, BienvenidosActivity.class );
-                startActivity(mainact);
-            }
-        });
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerAct = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(registerAct);
-            }
-        });
-
-
-    }
+//        final EditText etEmail  = (EditText) findViewById(R.id.editTextEmail);
+//        final EditText etPassword = (EditText) findViewById(R.id.editTextPassword);
+//        Button btnLogin = (Button) findViewById(R.id.entrar);
+//        Button btnRegister = (Button) findViewById(R.id.registrarse);
+//
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String Email = etEmail.getText().toString();
+//                String Contrasena = etPassword.getText().toString();
+//
+//                SharedPreferences preferences = getPreferences("MYPREFS", MODE_PRIVATE);
+//
+//                String emailDetalles = preferences.getString(Email + Contrasena + "data", "Email o Contrasena es incorrecto.");
+//                SharedPreferences.Editor editor = preferences.edit();
+//                editor.commit();
+//
+//                Intent mainact = new Intent(MainActivity.this, BienvenidosActivity.class );
+//                startActivity(mainact);
+//            }
+//        });
+//        btnRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent registerAct = new Intent(MainActivity.this, RegisterActivity.class);
+//                startActivity(registerAct);
+//            }
+//        });
+}
     @Override
     public void onBackPressed() {
 
     }
 
-//    public void clickButton(View view) {
-//        Intent intent;
-//
-//        switch (view.getId()) {
-//            case R.id.entrar:{
-//                login(view);
-//                break;
-//            }
-//            case R.id.registrarse:{
-//                intent = new Intent(this, RegisterActivity.class);
-//                startActivity(intent);
-//                break;
-//            }
-//
-//        }
-//
-//    }
+    public void clickButton(View view) {
+        Intent intent;
 
-//    public void login(View view) {
-//        emailValue = String.valueOf(editTextEmail.getText());
-//        passwordValue = String.valueOf(editTextPassword.getText());
-//
-//        for(int i = 0; i < validEmail.length ; i++) {
-//            if (emailValue.equals(validEmail[i]) && passwordValue.equals(validPassword[i])) {
-//                Intent intent = new Intent(this, BienvenidosActivity.class);
-//                startActivity(intent);
-//                return;
-//            }
-//        }
-//
-//        Toast.makeText(this, "Email o contraseña incorrectos!", Toast.LENGTH_SHORT).show();
-//    }
+        switch (view.getId()) {
+            case R.id.entrar:{
+                login(view);
+                break;
+            }
+            case R.id.registrarse:{
+                intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+
+    }
+
+    public void login(View view) {
+        emailValue = String.valueOf(editTextEmail.getText());
+        passwordValue = String.valueOf(editTextPassword.getText());
+
+        for(int i = 0; i < validEmail.length ; i++) {
+            if (emailValue.equals(validEmail[i]) && passwordValue.equals(validPassword[i])) {
+                Intent intent = new Intent(this, BienvenidosActivity.class);
+                startActivity(intent);
+                return;
+            }
+        }
+
+        Toast.makeText(this, "Email o contraseña incorrectos!", Toast.LENGTH_SHORT).show();
+    }
 }
