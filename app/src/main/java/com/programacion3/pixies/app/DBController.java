d12c60d7ecc71f50d3835abd9d8410fea4be7099
Renato Rodriguez <rodri_renato_98@hotmail.com>
2018-05-07 17:29:09 -0400

    Merge branch 'master' of C:\Users\Renato Rodriguez\AndroidStudioProjects\programacion3-pixies with conflicts.

--- app/src/main/java/com/programacion3/pixies/app/DBController.java
+++ app/src/main/java/com/programacion3/pixies/app/DBController.java
@@ -2,17 +2,10 @@ package com.programacion3.pixies.app;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 public class DBController extends SQLiteOpenHelper {
-    String nombre,categoria;
-    int precio,imagen;
 
 
     public DBController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -21,33 +14,13 @@ public class DBController extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE Productos (_id INTEGER PRIMARY KEY AUTOINCREMENT, Imagen INTEGER, Cantidad INTEGER, Precio INTEGER, Nombre TEXT, Categoria TEXT);");
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("Nombre",nombre);
-        contentValues.put("Precio",precio);
-        contentValues.put("Categoria",categoria);
-        contentValues.put("Imagen",imagen);
-        db.insert("Productos", null, contentValues);
+        db.execSQL("CREATE TABLE Productos (Nombre TEXT, Precio INTEGER, Categoria TEXT, Imagen INTEGER);");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS Productos;");
         onCreate(db);
-    }
-
-    public List<Producto> getAllProducts(){
-        List<Producto> productoList = new LinkedList<>();
-        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM Productos",null);
-        while(cursor.moveToNext()){
-            productoList.add(new Producto(cursor.getInt(1),cursor.getInt(2),cursor.getInt(3),cursor.getString(4), cursor.getString(5)));
-        }
-        return productoList;
-    }
-
-    // TODO: ADD FUNCTION TO GET ALL PRODUCTS BY CATEGORY
-    public List<Producto> getAllProductsByCategory(String category) {
-
-
     }
 }
